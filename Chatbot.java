cf0625d304238ea75d4bbcb767fc044032224a4e
24A3144414 <vallika7724@gmail.com>
2026-06-10 15:41:29 +0530

    Create Chatbot.java

---
+++ Chatbot.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Chatbot {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("AI Chatbot Started");
+        System.out.println("Type 'bye' to exit");
+
+        while (true) {
+
+            System.out.print("You: ");
+            String input = sc.nextLine().toLowerCase();
+
+            if (input.equals("hello")) {
+                System.out.println("Bot: Hi there!");
+            }
+            else if (input.equals("how are you")) {
+                System.out.println("Bot: I am fine.");
+            }
+            else if (input.equals("what is your name")) {
+                System.out.println("Bot: I am CodeAlpha Chatbot.");
+            }
+            else if (input.equals("bye")) {
+                System.out.println("Bot: Goodbye!");
+                break;
+            }
+            else {
+                System.out.println("Bot: Sorry, I don't understand.");
+            }
+        }
+
+        sc.close();
+    }
+}
